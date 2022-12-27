1dddcb0dc5084a14f1c52db3a12fbc10fc9dd644
FedorzhyveL <fzhyvel@gmail.com>
2022-12-27 09:00:08 +0300

    Added internet connectoin checker

---
+++ lib/core/platform/network_info.dart
@@ -0,0 +1,18 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  @override
+  Future<bool> get isConnected async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      return Future.value(true);
+    } else {
+      return Future.value(true);
+    }
+  }
+}
