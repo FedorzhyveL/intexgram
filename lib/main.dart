f4b5375ec9867e3638a45c360f7a43c3dd1f7d7e
FedorzhyveL <fzhyvel@gmail.com>
2023-01-18 14:49:49 +0300

    Added default app orientation and status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:intexgram/Presentation/theme/palette.dart';
 import 'package:intexgram/locator_service.dart';
 import 'Presentation/Routes/router.gr.dart';
 import 'firebase_options.dart';
@@ -10,6 +12,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // FirebaseAuth.instance.signOut();
+  // FirebaseAuth.instance.currentUser!.delete();
   runApp(MyApp());
 }
 
@@ -19,6 +23,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+      ],
+    );
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Palette.statusBarColor,
+      ),
+    );
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerDelegate: flutterRouter.delegate(),
