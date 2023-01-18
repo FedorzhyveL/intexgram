ad08a95aa7e0493b0e032bceac3c6e00c8ec3c2c
FedorzhyveL <fzhyvel@gmail.com>
2023-01-18 14:40:16 +0300

    Edited main screen, added bloc+freezed and loading screen

---
+++ lib/Presentation/Screens/main_screen/bloc/main_screen_state.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:intexgram/Domain/entities/person_entity.dart';
+
+part 'main_screen_state.freezed.dart';
+
+@freezed
+class MainScreenState with _$MainScreenState {
+  const factory MainScreenState.initial() = Initial;
+
+  const factory MainScreenState.userLoaded(
+    PersonEntity user,
+  ) = UserLoaded;
+}
--- lib/Presentation/Screens/main_screen/main_screen.dart
+++ lib/Presentation/Screens/main_screen/main_screen.dart
@@ -1,9 +1,15 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intexgram/Presentation/Routes/router.gr.dart';
-import 'package:intexgram/Presentation/Screens/main_screen/cubit/user_cubit.dart';
+import 'package:intexgram/Presentation/Screens/main_screen/bloc/main_screen_bloc.dart';
+import 'package:intexgram/Presentation/Screens/main_screen/loading_screen.dart';
+import 'package:intexgram/Presentation/theme/palette.dart';
 import 'package:intexgram/locator_service.dart';
+
+import 'bloc/main_screen_event.dart';
+import 'bloc/main_screen_state.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -12,91 +18,117 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  bool liked = false;
-  int likes = 321;
-  int bottomIndex = 0;
-  late final UserCubit cubit;
+  late final MainScreenBloc bloc;
 
   @override
   void initState() {
-    cubit = UserCubit(getCurrentPersons: serverLocator());
+    bloc = MainScreenBloc(serverLocator());
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => cubit..getUser(),
-      child: BlocBuilder<UserCubit, UserState>(
+      create: (_) => bloc..add(const LoadUser()),
+      child: BlocBuilder<MainScreenBloc, MainScreenState>(
         builder: (context, state) {
-          if (state is UserLoaded) {
-            return AutoTabsRouter(
-              routes: const [
-                MainPageRoute(),
-                SearchPageRoute(),
-                MainPageRoute(),
-                NotificationsPageRoute(),
-                ProfilePageRoute(),
+          return state.when(
+            initial: () => const LoadingScreen(),
+            userLoaded: (user) => AutoTabsRouter(
+              routes: [
+                const HomePageRoute(),
+                const SearchPageRoute(),
+                const AddPhotoRoute(),
+                const FavoritesPageRoute(),
+                ProfilePageRoute(userEmail: user.email),
               ],
               builder: (context, child, animation) {
                 final tabsRouter = AutoTabsRouter.of(context);
                 return Scaffold(
                   body: child,
                   bottomNavigationBar: BottomNavigationBar(
+                    backgroundColor: Palette.appBarColor,
+                    iconSize: 30,
+                    showSelectedLabels: false,
+                    showUnselectedLabels: false,
                     type: BottomNavigationBarType.fixed,
                     currentIndex: tabsRouter.activeIndex,
                     onTap: (value) {
                       tabsRouter.setActiveIndex(value);
                     },
-                    items: const [
-                      BottomNavigationBarItem(
-                        icon: Icon(
-                          Icons.home_filled,
-                          color: Colors.black,
-                        ),
+                    items: [
+                      const BottomNavigationBarItem(
                         label: 'Home',
-                      ),
-                      BottomNavigationBarItem(
                         icon: Icon(
-                          Icons.search_outlined,
-                          color: Colors.black,
+                          Icons.home_outlined,
+                          color: Palette.bottomNavigationIconColor,
                         ),
-                        label: 'Search',
+                        activeIcon: Icon(
+                          Icons.home_filled,
+                          color: Palette.bottomNavigationIconColor,
+                        ),
                       ),
-                      BottomNavigationBarItem(
+                      const BottomNavigationBarItem(
+                        label: 'Search',
+                        icon: Icon(
+                          Icons.search_rounded,
+                          color: Palette.bottomNavigationIconColor,
+                        ),
+                        activeIcon: ImageIcon(
+                          AssetImage("assets/icons/search_thick.png"),
+                          color: Palette.bottomNavigationIconColor,
+                        ),
+                      ),
+                      const BottomNavigationBarItem(
+                        label: 'New post',
                         icon: Icon(
                           Icons.add_box_outlined,
-                          color: Colors.black,
+                          color: Palette.bottomNavigationIconColor,
                         ),
-                        label: 'New post',
+                        activeIcon: Icon(
+                          Icons.add_box_rounded,
+                          color: Palette.bottomNavigationIconColor,
+                        ),
                       ),
-                      BottomNavigationBarItem(
+                      const BottomNavigationBarItem(
+                        label: 'Actions',
                         icon: Icon(
                           Icons.favorite_border_rounded,
-                          color: Colors.black,
+                          color: Palette.bottomNavigationIconColor,
                         ),
-                        label: 'Actions',
+                        activeIcon: Icon(
+                          Icons.favorite_rounded,
+                          color: Palette.bottomNavigationIconColor,
+                        ),
                       ),
                       BottomNavigationBarItem(
+                        label: 'Profile',
                         icon: CircleAvatar(
-                          backgroundColor: Colors.grey,
-                          radius: 12,
+                          backgroundColor: Palette.appBarColor,
+                          radius: 15,
                           child: CircleAvatar(
-                            radius: 120,
-                            backgroundImage:
-                                AssetImage('assets/photos/original.jpg'),
+                            radius: 14,
+                            foregroundImage: CachedNetworkImageProvider(
+                                user.profilePicturePath),
                           ),
                         ),
-                        label: 'Profile',
+                        activeIcon: CircleAvatar(
+                          backgroundColor: Palette.bottomNavigationIconColor,
+                          radius: 15,
+                          child: CircleAvatar(
+                            backgroundColor: Palette.appBarColor,
+                            radius: 14,
+                            foregroundImage: CachedNetworkImageProvider(
+                                user.profilePicturePath),
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 );
               },
-            );
-          } else {
-            return const CircularProgressIndicator();
-          }
+            ),
+          );
         },
       ),
     );
