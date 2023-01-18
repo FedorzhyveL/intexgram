2d4ddcaedc471b08483f1fc01181071a6a73b483
FedorzhyveL <fzhyvel@gmail.com>
2023-01-18 12:40:58 +0300

    Added "Theme" folder to store all themed data

--- lib/Presentation/theme/palette.dart
+++ lib/Presentation/theme/palette.dart
@@ -1,5 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  static const black = Colors.black;
+  //status bar
+  static const statusBarColor = Colors.white;
+
+  //app bar
+  static const appBarColor = Colors.white;
+  static const appBarTextColor = Colors.black;
+  static const appBarIconColor = Colors.black;
+
+  //
+  static const textColor = Colors.black;
+  static const borderSideColor = Colors.grey;
+
+  //post
+  static const likedPostLikeColor = Colors.red;
+  static const notLikedPostLikeColor = Colors.grey;
+  static const postFavoriteColor = Colors.black;
+
+  //profile
+  static const profileTabsIconColor = Colors.black;
+
+  static const profileUnselectedTabsColor = Color.fromRGBO(214, 214, 214, 1);
+  static const profileButtonDefautColor = Color.fromRGBO(238, 238, 238, 1);
+  static const profileButtonSecondColor = Colors.blue;
+
+  //or divider
+  static const orDividerColor = Colors.black;
+
+  //bottom navigation
+  static const bottomNavigationIconColor = Colors.black;
+
+  //sign button
+  static const signButtonColor = Color(0xFF90CAF9);
+  static const signTextColor = Colors.blue;
+
+  //comments page
+  static const disabledButtonColor = Color(0xFF90CAF9);
+  static const abledButtonColor = Colors.blue;
 }
